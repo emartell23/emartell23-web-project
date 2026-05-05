59e317b88e7ad64f809ff7e50e2e8531ac1ecfd3
Ernesto Martell <emartell@nmsu.edu>
2026-05-05 16:04:50 -0600

    Project 4 JS Dark Mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('greetingForm');
+  const greeting = document.getElementById("greetingMessage");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("username").value;
+    const color = document.getElementById("color").value;
+
+    greeting.textContent = `Welcome to my webpage, ${name}! Your favorite color is ${color}.`;
+    greeting.style.color = color;
+  });
+});
 const darkToggle = document.getElementById("darkToggle");
 
 darkToggle.addEventListener("click", function () {
